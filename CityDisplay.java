b1e570ec47769e53352b9be5deb3d82fedc4e00b
medidiparthiv <53120768+medidiparthiv@users.noreply.github.com>
2019-07-25 18:01:58 -0400

    Add files via upload

---
+++ CityDisplay.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    Scanner x = new Scanner(System.in);
+	    
+	    String[] y = new String [4];
+	    
+	    System.out.print("Enter City Here : ");
+	    y[0] = x.nextLine();
+	    
+	    System.out.print("Enter City Here : ");
+	    y[1] = x.nextLine();
+	    
+	    System.out.print("Enter City Here : ");
+	    y[2] = x.nextLine();
+	    
+	    System.out.print("Enter City Here : ");
+	    y[3] = x.nextLine();
+	    
+	    System.out.println(y[0]);
+	    System.out.println(y[1]);
+	    System.out.println(y[2]);
+	    System.out.println(y[3]);
+	}
+}
